12811300013dd940b4a69cab2c3bcfb0f08205c1
Sahzad Khan <110531834+mdsahbazkhan@users.noreply.github.com>
2024-07-10 15:49:33 +0530

    update (securityuser);

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,5 +1,144 @@
+// import conf from "../conf/conf";
+// import { Client, Databases, ID, Storage } from "appwrite";
+
+// export class Service {
+//   client = new Client();
+//   databases;
+//   bucket;
+
+//   constructor() {
+//     this.client
+//       .setEndpoint(conf.appwriteUrl)
+//       .setProject(conf.appwriteProjectId);
+//     this.databases = new Databases(this.client);
+//     this.bucket = new Storage(this.client);
+//   }
+
+//   //Document
+
+//   //create doc
+//   async createPost({ title, content, featuredImage, status, userId }) {
+//     try {
+//       const uniqueId = ID.unique();
+//       return await this.databases.createDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollectionId,
+//         uniqueId,
+//         {
+//           title,
+//           content,
+//           featuredImage,
+//           status,
+//           userId,
+//         }
+//       );
+//     } catch (error) {
+//       console.log("Appwrite service :: createPost :: error", error);
+//     }
+//   }
+
+//   //Update doc
+//   async updatePost(slug, { title, content, featuredImage, status }) {
+//     try {
+//       return await this.databases.updateDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollectionId,
+//         slug,
+//         {
+//           title,
+//           content,
+//           featuredImage,
+//           status,
+//         }
+//       );
+//     } catch (error) {
+//       console.log("Appwrite service :: updatePost :: error", error);
+//     }
+//   }
+
+//   async deletePost(slug) {
+//     try {
+//       await this.databases.deleteDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollectionId,
+//         slug
+//       );
+//       return true;
+//     } catch (error) {
+//       console.log("Appwrite service :: deletePost :: error", error);
+//       return false;
+//     }
+//   }
+
+//   //Get doc
+//   async getPost(slug) {
+//     try {
+//       return await this.databases.getDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollectionId,
+//         slug
+//       );
+//     } catch (error) {
+//       console.log("Appwrite serive :: getPost :: error", error);
+//       return false;
+//     }
+//   }
+
+//   //List doc
+
+//   async getPosts() {
+//     try {
+//       return await this.databases.listDocuments(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollectionId,
+       
+//       );
+//     } catch (error) {
+//       console.log("Appwrite serive :: getPosts :: error", error);
+//       return false;
+//     }
+//   }
+ 
+
+
+//   //File
+
+//   //file upload service
+//   async uploadFile(file) {
+//     try {
+//       return await this.bucket.createFile(
+//         conf.appwriteBucketId,
+//         ID.unique(),
+//         file
+//       );
+//     } catch (error) {
+//       console.log("Appwrite service :: uploadFile:: error", error);
+//       return false;
+//     }
+//   }
+//   //Delete File
+//   async deleteFile(fileId) {
+//     try {
+//       await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+//       return true;
+//     } catch (error) {
+//       console.log("Appwrite service :: deleteFile:: error", error);
+//       return false;
+//     }
+//   }
+//   //FilePreview
+//   getFilePreview(fileId) {
+//     return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+//   }
+  
+// }
+
+// const service = new Service();
+// export default service;
+
 import conf from "../conf/conf";
 import { Client, Databases, ID, Storage } from "appwrite";
+import authService from './auth'; // Import the AuthService
 
 export class Service {
   client = new Client();
@@ -14,9 +153,9 @@ export class Service {
     this.bucket = new Storage(this.client);
   }
 
-  //Document
+  // Document
 
-  //create doc
+  // Create doc
   async createPost({ title, content, featuredImage, status, userId }) {
     try {
       const uniqueId = ID.unique();
@@ -37,9 +176,15 @@ export class Service {
     }
   }
 
-  //Update doc
+  // Update doc
   async updatePost(slug, { title, content, featuredImage, status }) {
     try {
+      const post = await this.getPost(slug);
+
+      if (post.userId !== await this.getCurrentUserId()) {
+        throw new Error("Unauthorized");
+      }
+
       return await this.databases.updateDocument(
         conf.appwriteDatabaseId,
         conf.appwriteCollectionId,
@@ -58,6 +203,12 @@ export class Service {
 
   async deletePost(slug) {
     try {
+      const post = await this.getPost(slug);
+
+      if (post.userId !== await this.getCurrentUserId()) {
+        throw new Error("Unauthorized");
+      }
+
       await this.databases.deleteDocument(
         conf.appwriteDatabaseId,
         conf.appwriteCollectionId,
@@ -70,7 +221,7 @@ export class Service {
     }
   }
 
-  //Get doc
+  // Get doc
   async getPost(slug) {
     try {
       return await this.databases.getDocument(
@@ -84,26 +235,22 @@ export class Service {
     }
   }
 
-  //List doc
-
+  // List doc
   async getPosts() {
     try {
       return await this.databases.listDocuments(
         conf.appwriteDatabaseId,
-        conf.appwriteCollectionId,
-       
+        conf.appwriteCollectionId
       );
     } catch (error) {
       console.log("Appwrite serive :: getPosts :: error", error);
       return false;
     }
   }
- 
 
+  // File
 
-  //File
-
-  //file upload service
+  // File upload service
   async uploadFile(file) {
     try {
       return await this.bucket.createFile(
@@ -116,7 +263,8 @@ export class Service {
       return false;
     }
   }
-  //Delete File
+
+  // Delete File
   async deleteFile(fileId) {
     try {
       await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
@@ -126,13 +274,23 @@ export class Service {
       return false;
     }
   }
-  //FilePreview
+
+  // FilePreview
   getFilePreview(fileId) {
     return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
   }
-  
+
+  // Helper method to get the current user's ID
+  async getCurrentUserId() {
+    try {
+      const user = await authService.getCurrentUser();
+      return user.$id;
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUserId :: error", error);
+      throw new Error("Unable to get current user");
+    }
+  }
 }
 
 const service = new Service();
 export default service;
-
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,23 +2,106 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import appwriteService from "../appwrite/config";
+// import { Link, useNavigate } from 'react-router-dom';
+// import { Button } from "./index";
+
+// function PostCard({ $id, title, featuredImage }) {
+//   const [deleting, setDeleting] = useState(() => {
+//     // Initialize the deleting state from localStorage
+//     return localStorage.getItem(`deleting-${$id}`) === 'true' ? true : false;
+//   });
+//   const navigate = useNavigate();
+
+//   const deletePost = () => {
+//     if (deleting) return; // Prevent multiple deletion requests
+
+//     if (window.confirm("Are you sure you want to delete this post?")) {
+//       setDeleting(true); // Set deleting state to true to disable the button
+
+//       appwriteService.deletePost($id)
+//         .then((status) => {
+//           if (status) {
+//             appwriteService.deleteFile(featuredImage);
+//             navigate("/");
+//           }
+//         })
+//         .catch((error) => {
+//           console.error('Error deleting post:', error);
+//         })
+//         .finally(() => {
+//           // Reset deleting state regardless of deletion result
+//           setDeleting(false);
+//           // Clear the deleting state from localStorage
+//           localStorage.removeItem(`deleting-${$id}`);
+//         });
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Save the deleting state to localStorage whenever it changes
+//     localStorage.setItem(`deleting-${$id}`, deleting);
+//   }, [$id, deleting]);
+
+//   return (
+//     <div className='w-full bg-gray-300 rounded-xl p-4'>
+//       <Link to={`/post/${$id}`}>
+//         <div className='w-full  mb-3' >
+//           <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl w-full h-72' style={{height:'250px'}} />
+//         </div>
+//         <h2 className='text-lg font-bold '>{title}</h2>
+//       </Link>
+      
+//       <div className="flex justify-between items-center mt-2 flex-col sm:flex-row w-full">
+//   <button
+//     onClick={deletePost}
+//     className={`bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full sm:w-auto mt-2 sm:mt-0 ${deleting ? 'opacity-50 pointer-events-none' : ''}`}
+//   >
+//     {deleting ? 'Deleting...' : 'Delete'}
+//   </button>
+//   <Link
+//     to={`/edit-post/${$id}`}
+//     className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full sm:w-auto mt-2 sm:mt-0"
+//   >
+//     Edit
+//   </Link>
+// </div>
+
+//     </div>
+//   );
+// }
+
+// export default PostCard;
 import React, { useState, useEffect } from 'react';
 import appwriteService from "../appwrite/config";
+import authService from "../appwrite/auth";
 import { Link, useNavigate } from 'react-router-dom';
-import { Button } from "./index";
 
-function PostCard({ $id, title, featuredImage }) {
-  const [deleting, setDeleting] = useState(() => {
-    // Initialize the deleting state from localStorage
-    return localStorage.getItem(`deleting-${$id}`) === 'true' ? true : false;
-  });
+function PostCard({ $id, title, featuredImage, userId }) {
+  const [deleting, setDeleting] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const user = await authService.getCurrentUser();
+        setCurrentUser(user);
+        console.log(user)
+      } catch (error) {
+        console.error('Error fetching current user:', error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
   const deletePost = () => {
-    if (deleting) return; // Prevent multiple deletion requests
+    if (deleting) return;
 
     if (window.confirm("Are you sure you want to delete this post?")) {
-      setDeleting(true); // Set deleting state to true to disable the button
+      setDeleting(true);
 
       appwriteService.deletePost($id)
         .then((status) => {
@@ -31,43 +114,36 @@ function PostCard({ $id, title, featuredImage }) {
           console.error('Error deleting post:', error);
         })
         .finally(() => {
-          // Reset deleting state regardless of deletion result
           setDeleting(false);
-          // Clear the deleting state from localStorage
-          localStorage.removeItem(`deleting-${$id}`);
         });
     }
   };
 
-  useEffect(() => {
-    // Save the deleting state to localStorage whenever it changes
-    localStorage.setItem(`deleting-${$id}`, deleting);
-  }, [$id, deleting]);
-
   return (
     <div className='w-full bg-gray-300 rounded-xl p-4'>
       <Link to={`/post/${$id}`}>
-        <div className='w-full  mb-3' >
-          <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl w-full h-72' style={{height:'250px'}} />
+        <div className='w-full mb-3'>
+          <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl w-full h-72' style={{ height: '250px' }} />
         </div>
-        <h2 className='text-lg font-bold '>{title}</h2>
+        <h2 className='text-lg font-bold'>{title}</h2>
       </Link>
-      
-      <div className="flex justify-between items-center mt-2 flex-col sm:flex-row w-full">
-  <button
-    onClick={deletePost}
-    className={`bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full sm:w-auto mt-2 sm:mt-0 ${deleting ? 'opacity-50 pointer-events-none' : ''}`}
-  >
-    {deleting ? 'Deleting...' : 'Delete'}
-  </button>
-  <Link
-    to={`/edit-post/${$id}`}
-    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full sm:w-auto mt-2 sm:mt-0"
-  >
-    Edit
-  </Link>
-</div>
 
+      {currentUser && currentUser.$id === userId && ( // Only show buttons if current user matches userId
+        <div className="flex justify-between items-center mt-2">
+          <button
+            onClick={deletePost}
+            className={`bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2 ${deleting ? 'opacity-50 pointer-events-none' : ''}`}
+          >
+            {deleting ? 'Deleting...' : 'Delete'}
+          </button>
+          <Link
+            to={`/edit-post/${$id}`}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            Edit
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
